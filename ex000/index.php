12a42c55c7c221c320b8f8c5469ed16db0423d77
Bruno-Kempfer <brunopietrobomkempfer@gmail.com>
2024-08-06 21:57:01 -0300

    dia 1

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+    <h1>
+        <?php
+        
+           // echo "Ola mundo";
+
+            print("Ola, mundo!  \u{1F30E}");
+        
+        ?>
+    </h1>
+</body>
+</html>
